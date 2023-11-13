9d8df6e1ec6738053f4b3bb27b623f139f8aa675
Youth <boysoeng@naver.com>
2023-11-14 04:08:45 +0900

    feat(Menus): 주문한 메뉴 목록 생성 기능 구현
    - 주문한 메뉴, 개수 기반 생성
    - 메뉴 이름 존재 여부 검증
    - 메뉴 개수 0이하 여부 검증
    - 주문 총 메뉴 20개 초과 여부 검증

---
+++ src/main/java/christmas/model/Menus.java
@@ -0,0 +1,52 @@
+package christmas.model;
+
+import static christmas.constant.ErrorMessage.MENU_COUNT_ERROR;
+import static christmas.constant.ErrorMessage.MENU_NAME_ERROR;
+import static christmas.constant.ErrorMessage.MENU_SIZE_ERROR;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Menus {
+    private final Map<Menu, Integer> menus;
+
+    public Menus(Map<Menu, Integer> menus) {
+        validateNoSuchMenu(menus);
+        validateMenuCount(menus);
+        validateSize(menus);
+        this.menus = menus;
+    }
+
+    private void validateNoSuchMenu(Map<Menu, Integer> menus) {
+        if (menus.containsKey(Menu.NO_MENU)) {
+            throw new IllegalArgumentException(MENU_NAME_ERROR.getMessage());
+        }
+    }
+
+    private void validateMenuCount(Map<Menu, Integer> menus) {
+        List<Integer> counts = new ArrayList<>(menus.values());
+        if (hasBelowZero(counts)) {
+            throw new IllegalArgumentException(MENU_COUNT_ERROR.getMessage());
+        }
+
+    }
+
+    private boolean hasBelowZero(List<Integer> counts) {
+        return counts.stream()
+                .anyMatch(count -> count <= 0);
+    }
+
+    private void validateSize(Map<Menu, Integer> menus) {
+        if (getMenuCount(menus) > 20) {
+            throw new IllegalArgumentException(MENU_SIZE_ERROR.getMessage());
+        }
+    }
+
+    private int getMenuCount(Map<Menu, Integer> menus) {
+        return menus.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+}
