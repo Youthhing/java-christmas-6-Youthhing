3ef7f57e9e1a9831f67cf43c520bc2f1a62f1e99
Youth <boysoeng@naver.com>
2023-11-15 02:46:14 +0900

    feat(Badge): 할인 금액에 따른 배지 판별 기능 구현
    - 5천원 미만일 경우 없음

---
+++ src/main/java/christmas/model/Badge.java
@@ -0,0 +1,37 @@
+package christmas.model;
+
+import java.util.Arrays;
+
+public enum Badge {
+    NONE(0, 5000, "없음"),
+    STAR(5000, 10000, "별"),
+    TREE(10000, 20000, "트리"),
+    SANTA(20000, Integer.MAX_VALUE, "산타");
+
+    private final int minPrice;
+    private final int maxPrice;
+    private final String name;
+
+    Badge(int minPrice, int maxPrice, String name) {
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+        this.name = name;
+    }
+
+    public static Badge getBadgeByDiscount(int discount) {
+        return Arrays.stream(Badge.values())
+                .filter(badge -> badge.isInRange(discount))
+                .findFirst()
+                .orElse(NONE);
+
+    }
+
+    private boolean isInRange(int discount) {
+        return minPrice <= discount && discount < maxPrice;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
