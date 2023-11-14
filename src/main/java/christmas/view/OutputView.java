73112e3f22e8242a7ea080259173aeb0dd887de7
Youth <boysoeng@naver.com>
2023-11-14 17:53:01 +0900

    feat(OutputView): 안내메시지 출력 기능 추가
    - 날짜 입력 안내 메시지 출력
    - 혜택 미리보기 안내 메시지 출력

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,13 +3,23 @@ package christmas.view;
 public class OutputView {
 
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String INPUT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String INPUT_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String PREVIEW_BENEFIT_MESSAGE = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
     }
 
+    public void printInputDateMessage() {
+        System.out.println(INPUT_DATE_MESSAGE);
+    }
+
     public void printInputMenuMessage() {
         System.out.println(INPUT_MENU_MESSAGE);
+    }
+
+    public void printPreviewBenefitMessage() {
+        System.out.println(PREVIEW_BENEFIT_MESSAGE);
     }
 }
