933dd3f2a81d343764e00a49089c9655755bc29b
Youth <boysoeng@naver.com>
2023-11-14 21:00:55 +0900

    feat(EventDay): 날짜 구분 기능 구현
    - 해당 날짜가 평일, 주말인지 구분하는 기능
    - 해당 날짜가 특별 할인 날인지 구분하는 기능

--- src/main/java/christmas/model/EventDay.java
+++ src/main/java/christmas/model/EventDay.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import christmas.constant.ErrorMessage;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class EventDay {
@@ -10,6 +11,30 @@ public class EventDay {
     public EventDay(int date) {
         validateRange(date);
         this.date = LocalDate.of(2023, 12, date);
+    }
+
+    public boolean isWeekend() {
+        return isSaturday() || isFriDay();
+    }
+
+    public boolean isSpecialDay() {
+        return isSunDay() || isChristMas();
+    }
+
+    private boolean isFriDay() {
+        return date.getDayOfWeek() == DayOfWeek.FRIDAY;
+    }
+
+    private boolean isChristMas() {
+        return date.getDayOfMonth() == 25;
+    }
+
+    private boolean isSaturday() {
+        return date.getDayOfWeek() == DayOfWeek.SATURDAY;
+    }
+
+    private boolean isSunDay() {
+        return date.getDayOfWeek() == DayOfWeek.SUNDAY;
     }
 
     private void validateRange(int date) {
